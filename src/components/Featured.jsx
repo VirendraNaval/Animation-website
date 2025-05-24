3530c4dcb40e7faef19cdf7d5b5f38d7e4fa096a
VirendraNaval <151227972+VirendraNaval@users.noreply.github.com>
2025-05-24 19:29:29 +0530

    Update Featured.jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -16,7 +16,7 @@ function Featured() {
                 </div>
                 <div className='cardcontainer w-1/2 h-[75vh] '>
                     <div className='card w-full h-full rounded-2xl bg-green-500 overflow-hidden'>
-                        <img className='w-full h-full bg-cover' src="./public/img.jpg" alt="" />
+                        <img className='w-full h-full bg-cover' src="http://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
                     </div>
                 </div>
              </div>
