abfafcaa21063c93ca76054153b9990f11c3561b
VirendraNaval <151227972+VirendraNaval@users.noreply.github.com>
2025-05-24 18:43:45 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,11 +11,11 @@ function Cards() {
       </div>
       <div className="cardcontainer h-[75vh] w-1/2 flex gap-5 ">
         <div className='card relative flex items-center justify-center rounded-2xl w-full h-full bg-red-300 '>
-        <img className='w-28' src="cp.png" alt="" />
+        <img className='w-28' src="./public/cp.png" alt="" />
         <button className='absolute left-10 bottom-10 px-5 py-1 border-2 rounded-full'>&copy; 20222-2026</button>
         </div>
         <div className='card relative flex items-center justify-center rounded-2xl w-full h-full bg-purple-500 '>
-        <img className='w-28 transparent' src="cp.png" alt="" />
+        <img className='w-28 transparent' src="./public/cp.png" alt="" />
         <button className='absolute left-10 bottom-10 px-5 py-1 border-2 rounded-full'>&copy; 20222-2026</button>
         </div>
       </div>
